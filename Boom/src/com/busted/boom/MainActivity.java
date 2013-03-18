64d8379fae22c5c418225addd963154e2c2bff53
Derek Hopper <hopper.derek@gmail.com>
2013-03-17 21:22:24 -0400

    Switched the main activity to a web view that loads the Browse page.

---
+++ Boom/src/com/busted/boom/MainActivity.java
@@ -0,0 +1,76 @@
+package com.busted.boom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	private Button mButton;
+	private WebView mBrowseView;
+	private String mHostname;
+	private String mBrowseUrl;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		mHostname = getResources().getString(R.string.server_url);
+		mBrowseUrl = mHostname + getResources().getString(R.string.browse_uri);
+
+		setContentView(R.layout.activity_main);
+		
+		mButton = (Button) findViewById(R.id.take_picture);
+		mButton.setOnClickListener(mTakePictureListener);
+		
+		mBrowseView = (WebView) findViewById(R.id.browse_web_view);
+		mBrowseView.loadUrl(mBrowseUrl);
+
+	}
+
+	@Override
+	protected void onPause() {
+		
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		
+		super.onResume();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.action_bar, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch ( item.getItemId() ) {
+			case R.id.refresh:
+				mBrowseView.reload();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private OnClickListener mTakePictureListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View arg0) {
+			
+		}
+		
+	};
+	
+}
